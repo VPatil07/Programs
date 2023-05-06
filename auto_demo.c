5753d80beed3a1a91340c14b432f847ed996e3b1
VPatil07 <132768692+VPatil07@users.noreply.github.com>
2023-05-06 20:57:13 +0530

    Add files via upload

---
+++ auto_demo.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int Y;
+int Z = 10;         //global varibal
+
+int main()
+
+{
+    int A = 10;
+    int B;
+    auto int C = 10;
+    auto int D;
+ 
+    printf("%d\n",A);
+    printf("%d\n",B);
+    printf("%d\n",C);
+    printf("%d\n",D);
+    printf("%d\n",Y);
+    printf("%d\n",Z);
+
+    return 0;
+    
+}
